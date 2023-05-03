97ccee27b563daf01d4e7a39dc0fe1371bdd9328
Roshan kumar <113673793+Roshanrocked@users.noreply.github.com>
2023-05-04 04:34:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var cors = require("cors");
 var path = require("path");
 const connection = require("./db/config");
 const router = require("./router/data.model");
+const PORT = process.env.PORT || 3001;
 app.use(cors());
-
 app.use(express.json());
 
 app.use("/", router);
 
-app.listen(3001, () => {
-  console.log("listening on port 3001");
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
   connection();
 });
