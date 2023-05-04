55be10b5e4871a0324b022c93fe9aee98be62477
Roshan kumar <113673793+Roshanrocked@users.noreply.github.com>
2023-05-04 05:30:33 +0530

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 const connection = (mongoose.connection = async () => {
-  await mongoose.connect(
-    `mongodb+srv://roshan836sharma:MwTiF8WYxHhnZNeN@cluster0.fhe4sra.mongodb.net/test`
-  );
+  await mongoose.connect(`mongodb+srv://roshan836sharma:n50eOSWaSwYqpREm@cluster0.anxdawu.mongodb.net/test`);
 });
 
 module.exports = connection;
